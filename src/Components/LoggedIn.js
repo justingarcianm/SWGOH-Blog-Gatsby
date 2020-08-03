8c65c769aee905a5702cc02d6eaf63905c0d5150
justingarcianm <justingarcianm@gmail.com>
2020-08-02 23:30:40 -0600

    useEffect test

--- src/Components/LoggedIn.js
+++ src/Components/LoggedIn.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 
 const LoggedIn = () => {
-    const user = sessionStorage.getItem("name") || null
-    if (user) {
+    const [ key, setKey ] = useState(undefined)
+    useEffect(() => {
+       setKey(sessionStorage.getItem("name")) 
+    }, [])
+    
+    if (key) {
         return (
             <Fragment>
                 <li className="nav-item">
-                    <Link className="nav-link" activeClassName="active" to={`/author/${user}`}>{user}</Link>
+                    <Link className="nav-link" activeClassName="active" to={`/author/${key}`}>{key}</Link>
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to="/" onClick={() => sessionStorage.clear()}>Logout</Link>
